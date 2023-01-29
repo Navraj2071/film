0836fc5097480f16c5df9a18d3cb064c6ff423a9
Neha <neha@Navraj.local>
2023-01-29 21:04:55 +0530

    features

--- components/features/features.js
+++ components/features/features.js
@@ -4,6 +4,9 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { useTheme } from "@emotion/react";
 import Text from "../subcomponents/headings";
+import Card from "@mui/material/Card";
+import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
+import { width } from "@mui/system";
 
 const Features = () => {
   const theme = useTheme();
@@ -37,6 +40,64 @@ const Features = () => {
             sx={{ fontWeight: 600 }}
             gutterBottom
           />
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr",
+              gap: "20px",
+            }}
+          >
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={<AccessibilityNewIcon sx={{ fontSize: "100px" }} />}
+            />
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={<AccessibilityNewIcon sx={{ fontSize: "100px" }} />}
+            />
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={<AccessibilityNewIcon sx={{ fontSize: "100px" }} />}
+            />
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={<AccessibilityNewIcon sx={{ fontSize: "100px" }} />}
+            />
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={<AccessibilityNewIcon sx={{ fontSize: "100px" }} />}
+            />
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={<AccessibilityNewIcon sx={{ fontSize: "100px" }} />}
+            />
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={<AccessibilityNewIcon sx={{ fontSize: "100px" }} />}
+            />
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={<AccessibilityNewIcon sx={{ fontSize: "100px" }} />}
+            />
+            <FeatureCard
+              title="Feature Title"
+              text="This is a great feature to have in an application"
+              Icon={
+                <AccessibilityNewIcon
+                  sx={{ fontSize: "100px" }}
+                  color="primary"
+                />
+              }
+            />
+          </Box>
         </Box>
       </Container>
     </Box>
@@ -44,3 +105,23 @@ const Features = () => {
 };
 
 export default Features;
+
+const FeatureCard = (props) => {
+  const { title, text, Icon } = props;
+  const theme = useTheme();
+
+  return (
+    <Card
+      sx={{ width: "100%", backgroundColor: theme.palette.background.light }}
+      elevation={1}
+    >
+      {Icon}
+      <Typography variant="h3" gutterBottom color="text.medium">
+        {title}
+      </Typography>
+      <Typography variant="h6" gutterBottom color="text.low">
+        {text}
+      </Typography>
+    </Card>
+  );
+};
